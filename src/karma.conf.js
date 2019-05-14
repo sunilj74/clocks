192f088ce22fb0895d006e24dbe212f9fb31f19b
unknown <sunilj74@gmail.com>
2019-05-14 09:00:35 -0400

    switch to headless crhome

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -25,7 +25,15 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
+    // you can define custom flags
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },    
+    // browsers: ['Chrome'],
     singleRun: false,
     restartOnFileChange: true
   });
